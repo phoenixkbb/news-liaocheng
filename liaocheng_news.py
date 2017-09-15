163859f1112c7eceb23386dda7082704a4ba58b8
jing.yuan <jingyuan19880118@gmail.com>
2017-09-15 14:25:48 +0200

    set locale problem removed.

--- liaocheng_news.py
+++ liaocheng_news.py
@@ -7,12 +7,12 @@ import json
 from bs4 import BeautifulSoup
 
 # if unsupported locale setting reported, you can use locale -a to see whether zh_CN or zh_CN.utf-8 should be used
-locale.setlocale(locale.LC_TIME, "zh_CN.utf-8")
+#locale.setlocale(locale.LC_TIME, "zh_CN.utf-8")
 news_txt = open('news.txt', 'w')
 sys.stdout = news_txt
 filter_reg = re.compile('(.*epochtimes.*)|(.*dw.com.*)')
 
-TODAY = time.strftime("%Y年%-b月%-d日星期%a")
+#`TODAY = time.strftime("%Y年%-b月%-d日星期%a")
 TOPK = 10
 
 subprocess.Popen("bash liaocheng-news.sh",  shell=True).communicate()
